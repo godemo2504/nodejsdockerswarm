e2931dc59f6a2c66644ead8a3c7bab511b4e62cd
godemo2504 <godemo2504@gmail.com>
2025-08-09 07:20:49 +0200

    ajou

--- app/server.js
+++ app/server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send(`<html><head><meta charset="utf-8"><title>Node.js App</title></head><body><h1>Node.js App - ${process.env.APP_VERSION || 'dev'}</h1><p>Déployé via Docker Swarm ok ça marche top ok, c'est top, merci beaucoup. I love you</p></body></html>`);
+  res.send(`<html><head><meta charset="utf-8"><title>Node.js App</title></head><body><h1>Node.js App - ${process.env.APP_VERSION || 'dev'}</h1><p>Je veux Déployé via Docker Swarm ok ça marche top ok, c'est top, merci beaucoup. I love you</p></body></html>`);
 });
 
 app.listen(port, () => {
